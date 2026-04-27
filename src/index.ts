05f5c45adaab21386c670790eeeaec53eea7e0e4
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-27 16:20:01 +0300

    Export components and types from index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { GlobalNav }       from './GlobalNav';
+export type { GlobalNavProps, GlobalNavItem, TecApp } from './GlobalNav';
+export { TEC_COLORS, TEC_FONTS, TEC_RADIUS }          from './theme';
